3cec98e561f84eebde8e6fda6d590bfd424d89da
Amin Imani <aminpk99@gmail.com>
2021-06-29 00:35:21 +0430

    add header component

---
+++ src/layout/Header.jsx
@@ -0,0 +1,30 @@
+import { Grid, makeStyles } from "@material-ui/core";
+import React from "react";
+import { ReactComponent as Logo } from "../assets/svg/slack-new-logo.svg";
+
+const useStyles = makeStyles({
+  Header: {
+    backgroundColor: "#fff",
+    padding: ".6rem 2rem",
+    WebkitBoxShadow: "0px 14px 19px -16px rgba(0,0,0,0.2)",
+    MozBoxShadow: "0px 14px 19px -16px rgba(0,0,0,0.2)",
+    boxShadow: "0px 14px 19px -16px rgba(0,0,0,0.2)",
+    display: "flex",
+    alignItems: "center",
+  },
+});
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <Grid className={classes.Header} container>
+      <Grid lg={3}>
+        <Logo />
+      </Grid>
+      <Grid lg={6}>drop down</Grid>
+      <Grid lg={3}>register</Grid>
+    </Grid>
+  );
+};
+
+export default Header;
