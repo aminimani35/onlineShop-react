a67e78500b70bb704bfca8a3cdcd1deb3913e0c0
Amin Imani <aminpk99@gmail.com>
2021-07-08 00:36:44 +0430

    add search component ui

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,9 @@
-import { Grid, makeStyles } from "@material-ui/core";
+import { Grid, InputAdornment, makeStyles, TextField } from "@material-ui/core";
 import React from "react";
 import { ReactComponent as Logo } from "../assets/svg/slack-new-logo.svg";
 import AuthButton from "../components/AuthButton";
 import ShoppingCard from "../components/ShoppingCard";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles({
   Header: {
@@ -19,6 +20,12 @@ const useStyles = makeStyles({
     alignItems: "center",
     justifyContent: "space-evenly",
   },
+  Searchbar: {
+    backgroundColor: "#E5E5E5",
+    borderRadius: ".6rem",
+    width: "100%",
+    padding: ".25rem",
+  },
 });
 
 const Header = () => {
@@ -28,7 +35,20 @@ const Header = () => {
       <Grid lg={3}>
         <Logo />
       </Grid>
-      <Grid lg={6}>drop down</Grid>
+      <Grid lg={6}>
+        <TextField
+          placeholder="دنبال چی میگردی ؟"
+          className={classes.Searchbar}
+          InputProps={{
+            disableUnderline: true,
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
       <Grid className={classes.ShoppingCardWrapper} align="left" lg={3}>
         <AuthButton />
         <ShoppingCard />
